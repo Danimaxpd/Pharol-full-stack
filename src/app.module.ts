0d1bad81d72b297a883a453ec72383c6c740d0e6
DavidJm <david.jimenez@taximo.co>
2019-11-26 10:08:03 -0500

    Fixed connection issue

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,37 @@
+import * as dotenv from 'dotenv';
 import { Module } from '@nestjs/common';
-import { DatabaseModule } from '../database.module';
 
 import { AppService } from './services/app.service';
 import { AppController } from './controllers/app.controller';
 import { UserModule } from './modules/user.module';
-import { UserController } from './controllers/user/user.controller';
+import { UserService } from './services/user.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+dotenv.config();
+
+const mdPort = parseInt(process.env.MONGO_PORT.toString(), 0);
+const mdDB = process.env.MONGO_DB;
+const mdUser = process.env.MONGO_USER;
+const mdPass = process.env.MONGO_PASSWORD;
+const mdHost = process.env.MONGO_HOST;
 
 @Module({
-  imports: [DatabaseModule, UserModule],
-  controllers: [AppController, UserController],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mongodb',
+    host: mdHost,
+    port: mdPort,
+    username: mdUser,
+    password: mdPass,
+    database: mdDB,
+    logging: true,
+    synchronize: true,
+    migrationsRun: true,
+    entities: ['src/entities/**/*.ts'],
+    migrations: ['src/migrations/**/*.ts'],
+    cli: {
+      migrationsDir: 'src/migrations/**/*.ts',
+    },
+  }), UserModule],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -3,11 +3,10 @@ import { Injectable, Inject } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
 import { User } from '../entities/User';
-import { DatabaseModule } from '../../database.module';
 
 @Injectable()
 export class UserService extends TypeOrmCrudService<User> {
-  constructor(@Inject(DatabaseModule) connection, @InjectRepository(User) repo) {
+  constructor(@InjectRepository(User) repo) {
     super(repo);
   }
 }
